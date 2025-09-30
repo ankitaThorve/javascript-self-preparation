5f83a1307a0a413233f4623431797d05cf0e202c
Ankieta <ankietathorve@gmail.com>
2025-09-30 15:16:14 +0530

    js operators

---
+++ 04-operators.js
@@ -0,0 +1,50 @@
+// Assignment operator =
+let x = 10; // assign the value 10 to x
+
+// Addition operator +
+let a = 2;
+let b = 4;
+let c = a + b; // 6
+
+// Multiplication operator *
+let q = 2;
+let r = 10;
+let p = q * r;
+
+/*Types of JS Operators----
+1. Arithmatic
+2. Assignment
+3. Comparison
+4. Logical
+*/
+
+
+
+// String addition ---
+
+let text1 = "hello";
+let text2 = "world";
+let text3 = text1 + " " + text2;
+
+console.log(text3);
+
+// concatinate string
+let txt1 = "what a very ";
+txt1 += "nice day"; // here + is concatination operator
+
+console.log(txt1);
+
+// add string and number
+let e = 5 + 5;
+console.log(e);
+let f = "5" + 5;
+console.log(f);
+let g = "hello" + 5;
+console.log(g);
+
+//  Assignment Operators ---- += -= *= /= %= **=
+let z = 10
+z += 5
+console.log(z)
+
+// 
