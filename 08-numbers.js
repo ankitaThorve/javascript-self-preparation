fecc09ebb8a5df2ab481d6b8ff6d4986d0c28727
Ankieta <ankietathorve@gmail.com>
2025-10-06 16:06:24 +0530

    number methods

--- 08-numbers.js
+++ 08-numbers.js
@@ -36,4 +36,43 @@ while (myNum != Infinity) {
 }
 console.log(txt);
 
-console.log(typeof Infinity)
+console.log(typeof Infinity);
+
+// Number Methods ---
+// toString()
+let g = 123;
+console.log(g.toString() + "\n" + (100 + 23).toString());
+
+// toExponential()
+let k = 9.653;
+console.log(k.toExponential());
+
+// toFixed()
+console.log(k.toFixed());
+
+// toPrecision()
+console.log(k.toPrecision(2));
+
+// valueOf()
+console.log(k.valueOf());
+
+// Converting variables to numbers ----
+console.log(`${Number(true)}\n${Number(false)}\n${Number("10")}\n${Number(
+  "   10"
+)}\n${Number("10.33")}\n${Number("10,33")}\n${Number("John")}
+} `);
+
+// parseInt()
+// parseFloat()
+
+// Number.isInteger()
+console.log(Number.isInteger(10))
+console.log(Number.isInteger(10.59))
+
+console.log(Number.isFinite(10))
+
+console.log(Number.isNaN(9))
+
+console.log(Number.isSafeInteger(10))
+console.log(Number.isSafeInteger(012345678901234567890))
+
