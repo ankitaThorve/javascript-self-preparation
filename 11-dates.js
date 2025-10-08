8e358a63189020f4aeb7e23b6ca500443c583ff4
Ankieta <ankietathorve@gmail.com>
2025-10-08 16:18:30 +0530

    date get methods

--- 11-dates.js
+++ 11-dates.js
@@ -28,3 +28,48 @@ console.log(dd);
 const date = new Date("2025-07-19");
 console.log(date);
 
+// JS Get Date method- -
+console.log(d.getFullYear()); //4 digit number
+console.log(d.getMonth()); // a number (0-11)
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[d.getMonth()];
+console.log(month);
+
+console.log(d.getDate()); //a number (1-31)
+console.log(d.getHours()); // a number (0-23)
+console.log(d.getMinutes()) // 0-59
+console.log(d.getSeconds()) // 0-59
+console.log(d.getMilliseconds()) // 0-999
+console.log(d.getDay())// 0-6
+console.log(d.getTime())
+
+let ms = Date.now()
+console.log(ms)
+
+// Calculate the number of years since January 1, 1970:
+// Calculate milliseconds in a year
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
+
+// Divide Date.now() with a year
+let years = Math.round(Date.now() / year);
+console.log(years)
+
+let diff = d.getTimezoneOffset()
+console.log(diff)
