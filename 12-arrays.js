7a6999075a73da5a8237be411147d0b7364617e1
Ankieta <ankietathorve@gmail.com>
2025-10-14 16:10:00 +0530

    arrays are nothing but objects

--- 12-arrays.js
+++ 12-arrays.js
@@ -20,9 +20,46 @@ bikes[2] = "royal enfield";
 console.log(bikes);
 
 const company = new Array("google", "meta", "microsoft");
-console.log(company)
+console.log(company);
 
 // Accessing array elements ---- Array indexes start with 0.
-let carIndex = cars[0]
-console.log(`The first car is '${carIndex}'`)
+let carIndex = cars[0];
+console.log(`The first car is '${carIndex}'`);
 
+// Changing an array element ---
+company[0] = "jp morgan";
+console.log(company);
+
+// Converting an array to string ---
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.toString());
+
+// Arrays are Objects ----------
+// Arrays are a special type of objects. The typeof operator in JavaScript returns "object" for arrays.
+
+// But, JavaScript arrays are best described as arrays.
+
+// Arrays use numbers to access its "elements".
+
+const person = { firstName: "john", lastName: "doe", age: 46 };
+console.log(person.firstName); //Objects use names to access its "members"
+
+// the length property --- The length property is always one more than the highest array index.
+let length = fruits.length;
+console.log(length);
+
+console.log(fruits[0]); //frist array element
+console.log(fruits[fruits.length - 1]); //last array element
+
+fruits.push("Lemon");
+console.log(fruits.toString());
+fruits[fruits.length] = "Papaya";
+console.log(fruits.toString());
+fruits[9] = "Pomogranate";
+console.log(fruits);
+
+// JS new Array()----
+const points = new Array(40, 100, 420);
+// const point = [60]
+const point = new Array(60);
+console.log(point[0]);
