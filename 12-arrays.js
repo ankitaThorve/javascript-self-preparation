1fe807390124292cdd61ff7327510a45dd2628a9
Ankieta <ankietathorve@gmail.com>
2025-12-11 14:32:26 +0530

    rest operator

--- 12-arrays.js
+++ 12-arrays.js
@@ -408,23 +408,23 @@ let text1 = "";
 for (let x of keys) {
   text1 += x + "\n";
 }
-console.log(text1)
+console.log(text1);
 
 // entries() method returns an Array Iterator object with key/value pairs
-const f = fruits.entries()
-for(let x of f){
-  console.log(x)
+const f = fruits.entries();
+for (let x of f) {
+  console.log(x);
 }
 
 // Array with() method as a safe way to update elements in an array without altering the original array
-const myMonths = months.with(2,"March")
-console.log(myMonths)
+const myMonths = months.with(2, "March");
+console.log(myMonths);
 
 // JavaScript Array Spread (...)The ... operator expands an array into individual elements
-const arr1=[1,2,3]
-const arr2=[4,5,6]
-const arr3 = [...arr1, ...arr2]
-console.log(arr3)
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
 
 const q1 = ["Jan", "Feb", "Mar"];
 const q2 = ["Apr", "May", "Jun"];
@@ -432,14 +432,19 @@ const q3 = ["Jul", "Aug", "Sep"];
 const q4 = ["Oct", "Nov", "Des"];
 
 const year = [...q1, ...q2, ...q3, ...q4];
-console.log(year)
+console.log(year);
 
 // spread operator (...) can be used to copy an array
-const arr4= [...arr1]
-console.log(arr4)
+const arr4 = [...arr1];
+console.log(arr4);
 
 // used to pass arguments to a function
-let minValue = Math.min(...number1)
-let maxValue = Math.max(...number1)
-console.log(`Min = ${minValue} \nMax = ${maxValue}`)
+let minValue = Math.min(...number1);
+let maxValue = Math.max(...number1);
+console.log(`Min = ${minValue} \nMax = ${maxValue}`);
 
+// rest operator (...) allows us to destruct an array and collect the leftovers
+let a, rest1;
+const arrOne = [1, 2, 3, 4, 5, 6, 7, 8];
+[a, ...rest1] = arrOne;
+console.log(`The rest is: ${rest1}`);
