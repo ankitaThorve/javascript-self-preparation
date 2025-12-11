536fa0c6527401649b9fabe3e14473883b7b16c8
Ankieta <ankietathorve@gmail.com>
2025-12-11 16:22:33 +0530

    types array

---
+++ 13-typedArrays.js
@@ -0,0 +1,5 @@
+// Typed Arrays ----------------
+// Typed arrays was designed for handling Binary Data.Unlike arrays, typed arrays are buffers of Fixed Length.Typed arrays store elements of Fixed Types like 8-bit integers or 32-bit numbers.
+
+const myArr = new Uint8Array(5);
+console.log(`Array: ${myArr} \nBytes per element: ${myArr.BYTES_PER_ELEMENT}`);
